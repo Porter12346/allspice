21cbfdd72a31eabac5c43448ad825c28f6c02505
Porter Young <porteryoung2005@gmail.com>
2024-08-14 14:50:14 -0600

    table and model set up for recipes

---
+++ server/Models/Recipe.cs
@@ -0,0 +1,23 @@
+namespace allspice.Models;
+
+public class Recipe
+{
+
+    public int id { get; set; }
+
+    public DateTime createdAt { get; set; }
+
+    public DateTime updatedAt { get; set; }
+
+    public string title { get; set; }
+
+    public string instructions { get; set; }
+
+    public string img { get; set; }
+
+    public string category { get; set; }
+
+    public string creatorId { get; set; }
+
+    public Profile creator { get; set; }
+}
--- server/dbSetup.sql
+++ server/dbSetup.sql
@@ -1,8 +1,20 @@
-CREATE TABLE IF NOT EXISTS accounts(
-  id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
+CREATE TABLE IF NOT EXISTS accounts (
+    id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+    updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+    name varchar(255) COMMENT 'User Name',
+    email varchar(255) UNIQUE COMMENT 'User Email',
+    picture varchar(255) COMMENT 'User Picture'
+) default charset utf8mb4 COMMENT '';
+
+CREATE TABLE recipes (
+  id INT not NULL PRIMARY KEY,
   createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
   updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-  name varchar(255) COMMENT 'User Name',
-  email varchar(255) UNIQUE COMMENT 'User Email',
-  picture varchar(255) COMMENT 'User Picture'
-) default charset utf8mb4 COMMENT '';
+  title VARCHAR(255) NOT NULL,
+  instructions VARCHAR(5000),
+  img VARCHAR(1000),
+  category ENUM('breakfast', 'lunch', 'dinner', 'snack', 'dessert') NOT NULL DEFAULT 'lunch',
+  creatorId VARCHAR(255) NOT NULL, 
+  Foreign Key (creatorId) REFERENCES accounts (id) ON DELETE CASCADE
+) 
