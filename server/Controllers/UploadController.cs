6aef1681b65647e42c8cdb1644583a9bf24b5067
Porter Young <porteryoung2005@gmail.com>
2024-09-24 00:54:25 -0600

    added to upload controller

--- server/Controllers/UploadController.cs
+++ server/Controllers/UploadController.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Http;
+
 namespace allspice.Controllers;
 
 [ApiController]
@@ -5,10 +7,54 @@ namespace allspice.Controllers;
 
 public class UploadController : ControllerBase
 {
+    private readonly IAmazonS3 _s3Client;
+    private readonly UploadService _uploadService;
     private readonly Auth0Provider _auth0Provider;
 
-    public UploadController(Auth0Provider auth0Provider)
+    public UploadController(UploadService uploadService, IAmazonS3 s3Client, Auth0Provider auth0Provider)
     {
+        _uploadService = uploadService;
+        _s3Client = s3Client;
         _auth0Provider = auth0Provider;
+    }
+
+    [HttpPost]
+    [Authorize]
+
+    public async Task<IActionResult> UploadFile(IFormFile file)
+    {
+        try
+        {
+            Account userInfo = await _auth0Provider.GetUserInfoAsync<Account>(HttpContext);
+            string name = parseName(file.FileName, userInfo.Id);
+
+            // TODO integrate sharp
+
+            var request = new PutObjectRequest()
+            {
+                BucketName = "porteryoungbucket",
+                Key = name,
+                InputStream = file.OpenReadStream(),
+                ContentType = file.ContentType
+            };
+            request.Metadata.Add("Content-Type", file.ContentType);
+            var response = await _s3Client.PutObjectAsync(request);
+            return Ok($"https://porteryoungbucket.s3.us-west-2.amazonaws.com/{name}");
+        }
+        catch (Exception exception)
+        {
+            return BadRequest(exception.Message);
+        }
+    }
+
+    private static string parseName(string source, string userId)
+    {
+        string[] s = source.Split(' ');
+        string n = string.Join("_", s);
+        return $"{userId.TrimEnd('/')}/{n.Substring(0, n.LastIndexOf('.'))}";
+
+        // foreach (int c in source)
+        //     if(source[c] == ' ')source = source.Remove(c, 1);
+        // return source;
     }
 }
